2d29b159d1ee836d1a53724789354e0c33fcecbd
TheRealPieFace <34294484+TheRealPieFace@users.noreply.github.com>
2019-08-25 17:02:57 -0400

    Added Cinemachine

--- Assets/Pierre's Folder/Pierre's Scripts/Menu.cs
+++ Assets/Pierre's Folder/Pierre's Scripts/Menu.cs
@@ -1,23 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Player_Movement movement;
 
     public void KillPlayer()
     {
         Debug.Log("Kill Player");
+        movement.enabled = false;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
