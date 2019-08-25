da54a4f94e014ff4ac0ac74540708d04f5eead39
Jean-Paul <Jean_paul555@yahoo.com>
2019-08-24 20:41:54 -0400

    Movement Update
    Player can move, jump, and crouch.

---
+++ Assets/JP's Folder/JP's Scripts/Player_Controller.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Player_Controller : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 400f;
+    [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform ceilingCheck;
+    [SerializeField] private Collider2D crouchDisableCollider;
+
+    const float ceilingRadius = 1f;
+    const float groundedRadius = .2f;
+    private bool grounded;
+    private Rigidbody2D playerRigidBody;
+    private bool facingRight = true;
+    private Vector3 playerVelocity = Vector3.zero;
+
+    public UnityEvent OnLandEvent;
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    public BoolEvent OnCrouchEvent;
+    private bool wasCrouching = false;
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+
+        if (OnCrouchEvent == null)
+            OnCrouchEvent = new BoolEvent();
+    }
+
+    public void FixedUpdate()
+    {
+        bool wasGrounded = grounded;
+        grounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+    }
+
+    public void Move(float move, bool crouch, bool jump)
+    {
+        if (!crouch)
+        {
+            if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+            {
+                crouch = true;
+            }
+        }
+
+        if (grounded)
+        {
+            if (crouch)
+            {
+                if (!wasCrouching)
+                {
+                    wasCrouching = true;
+                    OnCrouchEvent.Invoke(true);
+                }
+
+                move *= crouchSpeed;
+
+                if (crouchDisableCollider != null)
+                {
+                    crouchDisableCollider.enabled = false;
+                }
+            }
+            else
+            {
+                if (crouchDisableCollider != null)
+                {
+                    crouchDisableCollider.enabled = true;
+                }
+                if (wasCrouching)
+                {
+                    wasCrouching = false;
+                    OnCrouchEvent.Invoke(false);
+                }
+            }
+        }
+
+        Vector3 targetVelocity = new Vector2(move * 10f, playerRigidBody.velocity.y);
+        playerRigidBody.velocity = Vector3.SmoothDamp(playerRigidBody.velocity, targetVelocity, ref playerVelocity, movementSmoothing);
+
+        if (move > 0 && !facingRight)
+        {
+            FlipSprite();
+        }
+        else if (move < 0 && facingRight)
+        {
+            FlipSprite();
+        }
+
+        if (grounded && jump)
+        {
+            grounded = false;
+            playerRigidBody.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    private void FlipSprite()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
