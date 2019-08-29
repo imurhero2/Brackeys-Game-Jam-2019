79124874c0326eae57996e31669268744a4a3b36
TheRealPieFace <34294484+TheRealPieFace@users.noreply.github.com>
2019-08-28 20:10:28 -0400

    Spikes are fuck

---
+++ Assets/Pierre's Folder/Pierre's Scripts/Spikes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+//    [SerializeField] private Game_Manager manager;
+    [SerializeField] private Animator playerAnimator;
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            //Game_Manager.KillPlayer();
+            playerAnimator.SetBool("diced", true);
+        }
+    }
+}
