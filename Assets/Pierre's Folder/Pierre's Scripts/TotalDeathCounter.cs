64004db64d0fb758154d92fd72bf2a2034435a6d
Jean-Paul <Jean_paul555@yahoo.com>
2019-08-31 01:00:55 -0400

    CreditsManager

---
+++ Assets/Pierre's Folder/Pierre's Scripts/TotalDeathCounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TotalDeathCounter : MonoBehaviour
+{
+    public TextMeshProUGUI deathCounterText;
+
+    private GameManager gManager;
+
+    void Awake()
+    {
+        gManager = FindObjectOfType<GameManager>();
+        deathCounterText.text = gManager.deathCount.ToString();
+    }
+}
